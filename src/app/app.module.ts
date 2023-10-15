da3db1d458ef425cbd2420a1d5c22021d6fc8129
Piotr Wiśniewski <piotrwisnie@wp.pl>
2023-10-15 22:59:14 +0200

    Final fixes and styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,11 @@ import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 import { ApiManagementServiceService } from './api-management-service.service';
 import { HttpClientModule } from '@angular/common/http';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 
 @NgModule({
   declarations: [
@@ -13,7 +18,12 @@ import { HttpClientModule } from '@angular/common/http';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    MatButtonModule,
+    MatIconModule,
+    MatInputModule,
+    MatSelectModule,
+    BrowserAnimationsModule,
   ],
   providers: [ApiManagementServiceService],
   bootstrap: [AppComponent]
